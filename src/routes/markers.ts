85905e3de3feabdaff4937b12ffdaad9a72feee9
John Evans <38507954+King-Mob@users.noreply.github.com>
2022-07-05 12:54:05 +0100

    add log for data group call

--- src/routes/markers.ts
+++ src/routes/markers.ts
@@ -10,6 +10,8 @@ type DataGroupRequest = Request & {
 };
 
 async function getUserDataGroups(request: DataGroupRequest, h: ResponseToolkit): Promise<ResponseObject> {
+    console.log("this gets called")
+
     const dataGroups = await findDataGroupsByUserId(request.auth.artifacts.user_id)
 
     return h.response(dataGroups);
