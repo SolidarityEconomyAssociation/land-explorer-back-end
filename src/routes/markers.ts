4a45736c816ac9d30bfee5c5dccb2285b69ed1d3
John Evans <38507954+King-Mob@users.noreply.github.com>
2022-07-19 13:38:43 +0100

    add new test route

--- src/routes/markers.ts
+++ src/routes/markers.ts
@@ -15,6 +15,13 @@ async function getUserDataGroups(request: DataGroupRequest, h: ResponseToolkit):
     return h.response(dataGroups);
 }
 
+async function test(request: Request, h: ResponseToolkit): Promise<ResponseObject> {
+
+
+    return h.response("hey there");
+}
+
 export const markerRoutes: ServerRoute[] = [
     { method: "GET", path: "/api/userdatagroups", handler: getUserDataGroups },
+    { method: "GET", path: "/api/test", handler: test },
 ];
